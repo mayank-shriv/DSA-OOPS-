092b7a768d843092178a243fcd4e001c1bed2ae2
Mayank <shrivastavamayank01922@gmail.com>
2025-08-20 11:11:08 +0530

    Rewrite the merge code

--- Dsa/SortingAlgo/MergeSort.py
+++ Dsa/SortingAlgo/MergeSort.py
@@ -1,31 +1,24 @@
-# First merge two sorted array
-def merge(list1,list2):
-    result=[]
-    p,q=0,0
-    n,m= len(list1),len(list2)
-    while (p<n and q<m):
-        if list1[p]<=list2[q]:
-            result.append(list1[p])
-            p+=1   
-        else:
-            result.append(list2[q])    
-            q+=1   
-    if p<n:
-        while p<n:
-                result.append(list1[p])
-                p+=1
-    else:
-        while q<m:
-                result.append(list2[q])
-                q+=1
-
-    print(list(set(result)))
-
-
-list1 = [1,5,9,109,244]
-list2 = [8,9,10,20,120]
-merge(list1,list2)
-
-
 # Merge Sort 
 
+def mergeSort(list1, list2):
+    p,q = 0,0
+    n,m = len(list1), len(list2)
+    result = []
+    while p<n and q<m:
+        if list1[p]>list2[q]:
+            result.append(list2[q])
+            q+=1
+        else:
+            result.append(list1[p])
+            p+=1
+    while p<n:
+        result.append(list1[p])
+        p+=1  
+    while q<m:
+        result.append(list2[q])
+        q+=1
+    print(result)
+
+list1 = [12,23,435,456,5657,43435]
+list2 = [1,2,3,4,5,6,6,87,899]
+mergeSort(list1,list2)
